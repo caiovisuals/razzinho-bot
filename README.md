787df1c8ff46c9d72d4dbe895dd31abb216f16bf
caiothevisual <68815383+caiovisuals@users.noreply.github.com>
2026-03-19 18:44:32 -0300

    index

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ Bot do Discord pro servidor World of Razzar! Feito com muita dedicação pro meu
 
 ## Dependências
 
+- **[discord.js] - ^14.14.1
+- **[dotenv] - ^16.4.5
+- **[nodemon] - ^3.1.14
 
 by caiothevisual
 #caiobavisuals #discord #bot #razzar #javascript
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import { Client, GatewayIntentBits } from "discord.js"
 import "dotenv/config"
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+})
+
+client.once("ready", () => {
+    console.log(`Logado como ${client.user.tag}`)
+})
+
+client.login(process.env.DISCORD_TOKEN)
